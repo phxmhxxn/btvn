64679225db155eb8f4ca7e84bae1b68881e92bed
phxmhxxn <92kkid@gmail.com>
2020-12-03 22:12:48 +0700

    dsadas

--- btvn/html-1-12-2020/js/mysite.js
+++ btvn/html-1-12-2020/js/mysite.js
@@ -1,85 +1,55 @@
-var slideIndex = 1;     
+var slides = document.getElementsByClassName("mySlides");
+var dots = document.getElementsByClassName("dot");
+var slideIndex = 0;
+window.onload = showSlides;
 
-
-function plusSlides(n) {  
-  
-  
-    if (n==1) {
-      
+function plusSlides(n) {
+  if (n == 1) {
     var i;
-    var slides = document.getElementsByClassName("mySlides"); 
-    m = slideIndex;
-    slides[slideIndex-1].className = "mySlides slideOut";   
+    slides[slideIndex - 1].className = "mySlides slideOut";
     slideIndex += n;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    
-    
-    slides[slideIndex-1].className = "mySlides slideIn";   
-   
-    }
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].className = "mySlides slideIn";
+  }
 
-    if (n==-1) {
-      var i;
-      var slides = document.getElementsByClassName("mySlides"); 
-      m = slideIndex;
-      slides[slideIndex-1].className = "mySlides slideOutR";   
-      slideIndex += n;
-      if (slideIndex < 1) {slideIndex = slides.length}
-      
-      
-      slides[slideIndex-1].className = "mySlides slideInR";   
-     
-      }
+  if (n == -1) {
+    var i;
+    slides[slideIndex - 1].className = "mySlides slideOutR";
+    slideIndex += n;
+    if (slideIndex < 1) { slideIndex = slides.length }
+    slides[slideIndex - 1].className = "mySlides slideInR";
+  }
 
-
-      var dots = document.getElementsByClassName("dot");
-      for (i = 0; i < dots.length; i++) {
-          dots[i].className = dots[i].className.replace(" active", "");
-      } 
-      dots[slideIndex-1].className += " active";
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  dots[slideIndex - 1].className += " active";
 
 }
- 
-function currentSlide(n) { 
-  var slides = document.getElementsByClassName("mySlides");  
-  slides[slideIndex-1].className = "mySlides slideOut";   
-  
 
+function currentSlide(n) {
+  slides[slideIndex - 1].className = "mySlides slideOut";
   slideIndex = n;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  
-  
-  slides[slideIndex-1].className = "mySlides slideIn";   
-  
-  var dots = document.getElementsByClassName("dot");
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].className = "mySlides slideIn";
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  } 
-  dots[slideIndex-1].className += " active";
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  dots[slideIndex - 1].className += " active";
 }
 
 function showSlides() {
   var i;
-  var slides = document.getElementsByClassName("mySlides");  
-  slides[slideIndex-1].className = "mySlides slideOut";   
-  
-
+  if (slideIndex == 0) {
+    slideIndex = slides.length;
+  }
+  slides[slideIndex - 1].className = "mySlides slideOut";
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  
-  
-  slides[slideIndex-1].className = "mySlides slideIn";    
-  
-  var dots = document.getElementsByClassName("dot");
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].className = "mySlides slideIn";
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  } 
-  dots[slideIndex-1].className += " active";
-  
-
-   
-  setTimeout(showSlides, 5000);  
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 5000);
 }
-  
-
-window.onload = showSlides;
--- btvn/html-1-12-2020/js/order.js
+++ btvn/html-1-12-2020/js/order.js
@@ -1,10 +1,13 @@
 var isShow = false;
 function toggleTerms() {
     var collapsibleContent = document.getElementById("collapsibleContent")
-    collapsibleContent.style.display = isShow == true ? "none": "block"
+    var term = document.getElementById("term")
+    collapsibleContent.style.display = isShow == true ? "none" : "block"
+    collapsibleContent.style.display = isShow == true ? term.innerText = "Show" : term.innerText = "Hide"
     isShow = !isShow
+
 }
 
-function success() { 
+function success() {
     alert("Your order saved successfully!");
 }
